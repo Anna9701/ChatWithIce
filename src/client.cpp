5aedd43d6589cc8bb6d1dbe352c67c7a6fc91abc
Anna Wyrwal <202340@edu.p.lodz.pl>
2018-04-04 15:38:40 -0500

    Ready working sample

--- src/client.cpp
+++ src/client.cpp
@@ -1,6 +1,29 @@
 #include <Ice/Ice.h>
 #include "chat.h"
 
-int main () {
-    return 0;
+using namespace std;
+using namespace Chat;
+
+int main(int argc, char* argv[]) {
+    int status = 0;
+    Ice::CommunicatorPtr ic;
+    try {
+        ic = Ice::initialize(argc, argv);
+        Ice::ObjectPrx base = ic->stringToProxy("SimpleUser:default -p 10000");
+        UserPrx user = UserPrx::checkedCast(base);
+        if (!user)
+            throw "Invalid proxy";
+        cout << user->getName() << endl;
+    } catch (const Ice::Exception& ex) {
+        cerr << ex << endl;
+        status = 1;
+    } catch (const char* msg) {
+        cerr << msg << endl;
+        status = 1;
+    }
+
+    if (ic)
+        ic->destroy();
+
+    return status;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,47 @@
 #include <Ice/Ice.h>
 #include "chat.h"
 
-int main () {
-    return 0;
+using namespace std;
+using namespace Chat;
+
+class UserI : public User {
+    public:
+        UserI(string n) : name(n){};
+        virtual string getName(const Ice::Current&);
+        virtual void SendMessage(const RoomPrx&, const UserPrx&, const string&, const ::Ice::Current& = ::Ice::Current()) {} //TODO
+        virtual void SendPrivateMessage(const UserPrx&, const string&, const ::Ice::Current& = ::Ice::Current()) {}
+    private:
+        string name;
+};
+
+string UserI::getName(const Ice::Current&) { 
+    return name;
+}
+
+int main(int argc, char* argv[]) {
+    int status = 0;
+    Ice::CommunicatorPtr ic;
+    try {
+        ic = Ice::initialize(argc, argv);
+        Ice::ObjectAdapterPtr adapter = ic->createObjectAdapterWithEndpoints("SimpleUserAdapter", "default -p 10000");
+        Ice::ObjectPtr object = new UserI("Sample");
+        adapter->add(object, ic->stringToIdentity("SimpleUser"));
+        adapter->activate();
+        ic->waitForShutdown();
+    } catch (const Ice::Exception& e) {
+        cerr << e << endl;
+        status = 1;
+    } catch (const char* msg) {
+        cerr << msg << endl;
+        status = 1;
+    }
+    if (ic) {
+        try {
+            ic->destroy();
+        } catch (const Ice::Exception& e) {
+            cerr << e << endl;
+            status = 1;
+        }
+    }
+    return status;
 }
